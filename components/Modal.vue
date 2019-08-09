aa7d37378115d0d1bcd05c578e53b6e8209d774f
johnenrick <johnenrickplenos@gmail.com>
2019-08-09 19:23:16 +0800

    Modal Component

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,8 +1,44 @@
 <template>
-  <div></div>
+  <div>
+    <div ref="modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div v-bind:class="'modal-' + size" class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel"><fa :icon="icon" /> {{title}} </h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <slot name="body"></slot>
+          </div>
+          <div class="modal-footer">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-
+  props: {
+    icon: {
+      type: String,
+      default: null
+    },
+    title: {
+      type: String,
+      default: null
+    },
+    size: {
+      type: String,
+      default: 'md'
+    }
+  },
+  methods: {
+    _open(){
+      $(this.$refs.modal).modal('show')
+    }
+  }
 }
 </script>
