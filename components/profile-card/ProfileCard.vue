e2b3b6b6e3dd5f6975f84a926e9d393a4fa7761e
johnenrick <johnenrickplenos@gmail.com>
2019-10-09 23:00:56 +0800

    Profile Card

---
+++ components/profile-card/ProfileCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-show="isOpen">
+    Profile
+  </div>
+</template>
+<script>
+export default{
+  props: {
+    id: Number, //Id the of the user
+    profile_picture_file_name: String, // the file name of the profile picture
+    full_name: String,
+    job_title: String
+  },
+  data(){
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    _toggle(){ // open if close, close if open
+      if(this.isOpen){
+        this._close()
+      }else{
+        this._open()
+      }
+    },
+    _open(){ // open the card
+      /* TODOs
+        Other information should only be loaded once the card is open and it should only be loaded once
+      */
+      this.isOpen = true
+    },
+    _close(){ // close the card
+      this.isOpen = false
+    }
+  }
+}
+</script>
