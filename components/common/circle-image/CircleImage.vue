0b334e7e6199a81bd660495ff69549c28bf474fd
Ralph Christopher Galido <rcbgalido@tutanota.com>
2019-10-10 16:54:02 +0800

    implemented CircleImage component

---
+++ components/common/circle-image/CircleImage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <img class="rounded-circle" style="height: 21px; width: 21px;" :src="imagePath"/>
+    <span v-if="label">&nbsp;{{ label }}</span>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    imagePath: String,
+    label: String
+  }
+}
+</script>
