841fc75e897ae32317a30764a9effd25ec30d605
Ralph Christopher Galido <rcbgalido@tutanota.com>
2019-10-14 19:30:44 +0800

    updated ProfileCard component

--- components/profile-card/ProfileCard.vue
+++ components/profile-card/ProfileCard.vue
@@ -7,51 +7,55 @@
             <img class="h-100 w-100" :src="profile_picture_file_name" />
           </div>
           <div class="profile-main-info">
-            <strong>{{ full_name }}</strong><br/>
-            {{ job_title }}<br/>
+            <span class="font-weight-bold" v-if="full_name">
+              {{ full_name }}<br/>
+            </span>
+            <span v-if="job_title">
+              {{ job_title }} <br/>
+            </span>
             Company A<br/>
             <a class="color-blue" href="#" style="text-decoration: none;">Unfollow Me</a><br/>
             <div class="profile-links">
-              <a href="#" title="Video Call"><fa class="fa-lg color-gray" icon="video" /></a>&nbsp;
-              <a href="#" title="Message"><fa class="fa-lg color-gray" icon="pen-square" /></a>&nbsp;
-              <a href="#" title="Chatter"><fa class="fa-lg color-gray" icon="comments" /></a>&nbsp;
-              <fa class="fa-lg color-blue" icon="address-book" />&nbsp;
-              <a href="#" title="Profile"><fa class="fa-lg color-gray" icon="user" /></a>
+              <a href="#" title="Video Call"><fa class="fa-lg color-gray text-hover-primary" icon="video" /></a>&nbsp;
+              <a href="#" title="Message"><fa class="fa-lg color-gray text-hover-primary" icon="pen-square" /></a>&nbsp;
+              <a href="#" title="Chatter"><fa class="fa-lg color-gray text-hover-primary" icon="comments" /></a>&nbsp;
+              <fa class="fa-lg text-primary" icon="address-book" />&nbsp;
+              <a href="#" title="Profile"><fa class="fa-lg color-gray text-hover-primary" icon="user" /></a>
             </div>
           </div>
         </div>
         <div class="profile-other-info pt-2">
           <table>
             <tr>
-              <td class="color-blue"><strong>Office</strong></td>
+              <td>Office</td>
               <td>8912345612</td>
             </tr>
             <tr>
-              <td class="color-blue"><strong>Cell</strong></td>
+              <td>Cell</td>
               <td>98765234</td>
             </tr>
             <tr>
-              <td class="color-blue"><strong>Email</strong></td>
+              <td>Email</td>
               <td>intra1@yahoo.com</td>
             </tr>
             <tr>
-              <td class="color-blue"><strong>Facebook</strong></td>
+              <td>Facebook</td>
               <td>tesasdd</td>
             </tr>
             <tr>
-              <td class="color-blue"><strong>Twitter</strong></td>
+              <td>Twitter</td>
               <td>dasdasdas</td>
             </tr>
             <tr>
-              <td class="color-blue pr-3"><strong>Instagram</strong></td>
+              <td class="pr-3">Instagram</td>
               <td>dasdas</td>
             </tr>
             <tr>
-              <td class="color-blue"><strong>Linkedin</strong></td>
+              <td>Linkedin</td>
               <td>dasdasdas</td>
             </tr>
             <tr>
-              <td class="color-blue"><strong>Address</strong></td>
+              <td>Address</td>
               <td>Basak Pardo, Cebu City</td>
             </tr>
           </table>
@@ -100,10 +104,6 @@ export default {
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
-}
-
-.profile-main-info a:hover, .profile-main-info .fa-lg:hover, .color-blue {
-  color: #2b7bb9;
 }
 
 .color-gray {
@@ -162,6 +162,10 @@ export default {
     float: left;
     margin: 0 0 0 35px;
     width: 100%;
+}
+
+.profile-other-info > table td:nth-child(odd) {
+  font-weight: bold;
 }
 
 </style>
