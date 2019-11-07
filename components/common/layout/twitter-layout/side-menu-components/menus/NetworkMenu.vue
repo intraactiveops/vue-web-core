7fc8a932d49082ff26de74b8f990d5dd4021058f
johnenrick <johnenrickplenos@gmail.com>
2019-11-07 23:08:10 +0800

    Network Menu

--- components/common/layout/twitter-layout/side-menu-components/menus/NetworkMenu.vue
+++ components/common/layout/twitter-layout/side-menu-components/menus/NetworkMenu.vue
@@ -1,7 +1,6 @@
 <template>
   <ul class="list-group list-group-flush">
     <li class="list-group-item borderless py-1 px-2 font-weight-bold">Following</li>
-    <li class="list-group-item borderless py-1 px-2 font-weight-bold">Community</li>
     <li class="list-group-item borderless py-1 px-2 "><span class="font-weight-bold">My Networks</span> <a href="#" class="float-right">View All</a></li>
     <li class="list-group-item borderless py-1 px-2">Dev Team 51 <span class=" float-right badge text-white badge-info badge-pill">5</span></li>
     <li class="list-group-item borderless py-1 px-2">Team Leads <span class=" float-right badge text-white badge-info badge-pill">3</span></li>
