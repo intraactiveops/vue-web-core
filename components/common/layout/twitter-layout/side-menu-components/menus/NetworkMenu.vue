cfff001d7b1cf852ba1e0561765ab9d227c8ac3f
johnenrick <johnenrickplenos@gmail.com>
2019-11-19 23:02:41 +0800

    Small updates on fontawesome and network menu

--- components/common/layout/twitter-layout/side-menu-components/menus/NetworkMenu.vue
+++ components/common/layout/twitter-layout/side-menu-components/menus/NetworkMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item borderless py-1 px-2 "><span class="font-weight-bold">Mine</span> <a href="#" class="float-right">View All</a></li>
+    <li class="list-group-item borderless py-1 px-2 "><span class="font-weight-bold">Mine</span> <router-link to="/network-list" class="float-right"  title="View All Channel">View All</router-link></li>
     <li class="list-group-item borderless py-1 px-2">Dev Team 51 <span class=" float-right badge text-white badge-info badge-pill">5</span></li>
     <li class="list-group-item borderless py-1 px-2">Team Leads <span class=" float-right badge text-white badge-info badge-pill">3</span></li>
     <li class="list-group-item borderless py-1 px-2"><span class="font-weight-bold">Joined</span> <!-- <a href="#" class="float-right">View All</a> --></li>
