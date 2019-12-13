f8177a3a306167aa5f5ecb73f6957497ae121a66
Ralph Christopher Galido <rcbgalido@tutanota.com>
2019-12-14 00:03:21 +0800

    Update NetworkMenu.vue

--- components/common/layout/twitter-layout/side-menu-components/menus/NetworkMenu.vue
+++ components/common/layout/twitter-layout/side-menu-components/menus/NetworkMenu.vue
@@ -7,16 +7,16 @@
       <router-link to="/network/1"> Dev Team 51 <span class=" float-right badge text-white badge-info badge-pill">5</span></router-link>
     </li>
     <li class="list-group-item borderless py-1 px-0">
-      <router-link to="/network/1"> Team Leads <span class=" float-right badge text-white badge-info badge-pill">3</span></router-link>
+      <router-link to="/network/2"> Team Leads <span class=" float-right badge text-white badge-info badge-pill">3</span></router-link>
     </li>
     <li class="list-group-item borderless py-1 px-0">
       <span class="font-weight-bold">Joined</span>
     </li>
     <li class="list-group-item borderless py-1 px-0">
-      <router-link to="/network/1">IT Department</router-link>
+      <router-link to="/network/3">IT Department</router-link>
     </li>
     <li class="list-group-item borderless py-1 px-0">
-      <router-link to="/network/1">Designers</router-link>
+      <router-link to="/network/4">Designers</router-link>
     </li>
   </ul>
 </template>
