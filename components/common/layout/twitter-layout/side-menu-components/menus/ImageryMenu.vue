6e1abefbe3794a9162ce362115e71c392b28cab1
johnenrick <johnenrickplenos@gmail.com>
2020-01-16 00:32:39 +0800

    Update ImageryMenu.vue
    clickable

--- components/common/layout/twitter-layout/side-menu-components/menus/ImageryMenu.vue
+++ components/common/layout/twitter-layout/side-menu-components/menus/ImageryMenu.vue
@@ -1,13 +1,29 @@
 <template>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item py-1 px-2">Satellite </li>
-    <li class="list-group-item py-1 px-2">Drone </li>
-    <li class="list-group-item py-1 px-2">Battlefield </li>
-    <li class="list-group-item py-1 px-2">Airspace </li>
-    <li class="list-group-item py-1 px-2">Ground Forces </li>
-    <li class="list-group-item py-1 px-2">Special Operations </li>
-    <li class="list-group-item py-1 px-2">Intelligence </li>
-    <li class="list-group-item py-1 px-2">Geo Teams </li>
+    <li class="list-group-item py-1 px-2">
+      <router-link :to="'/imagery/' + 1">Satellite</router-link>
+    </li>
+    <li class="list-group-item py-1 px-2">
+      <router-link :to="'/imagery/' + 1">Drone</router-link>
+    </li>
+    <li class="list-group-item py-1 px-2">
+      <router-link :to="'/imagery/' + 1">Battlefield</router-link>
+    </li>
+    <li class="list-group-item py-1 px-2">
+      <router-link :to="'/imagery/' + 1">Airspace</router-link>
+    </li>
+    <li class="list-group-item py-1 px-2">
+      <router-link :to="'/imagery/' + 1">Ground Forces</router-link>
+    </li>
+    <li class="list-group-item py-1 px-2">
+      <router-link :to="'/imagery/' + 1">Special Operations</router-link>
+    </li>
+    <li class="list-group-item py-1 px-2">
+      <router-link :to="'/imagery/' + 1">Intelligence</router-link>
+    </li>
+    <li class="list-group-item py-1 px-2">
+      <router-link :to="'/imagery/' + 1">Geo Teams</router-link>
+    </li>
   </ul>
 </template>
 <script>
